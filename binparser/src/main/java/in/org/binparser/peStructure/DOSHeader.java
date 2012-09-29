e0ea60661cf8cd31c96c112ac666f9ca698ac1d8
rajdip <rajdips@gmail.com>
2012-09-29 01:23:15 +0100

    creating structure

---
+++ binparser/src/main/java/in/org/binparser/peStructure/DOSHeader.java
@@ -0,0 +1,11 @@
+package in.org.binparser.peStructure;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rajdip
+ * Date: 29/09/12
+ * Time: 01:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class DOSHeader {
+}
